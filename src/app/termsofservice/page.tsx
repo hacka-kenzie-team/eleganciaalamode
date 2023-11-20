6f8f1bdde49a43486fe0e20cc68ebba6300f44da
svnoliveira <srkuran@gmail.com>
2023-11-20 09:50:15 -0300

    feat: adicionado páginas de privacy e terms of use

---
+++ src/app/termsofservice/page.tsx
@@ -0,0 +1,44 @@
+
+
+export default function TermsOfService() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <section>
+        <h1 className="text-lg">Termos de Serviço</h1>
+
+        <p className="text-xs">Bem-vindo à nossa loja virtual de comércio eletrônico construída para fins de teste. Estes Termos de Serviço regem o seu uso deste site e quaisquer serviços relacionados oferecidos por nós.</p>
+
+        <p className="text-xs">Por favor, note que nosso site é apenas para fins de demonstração. Todos os dados neste site, incluindo informações sobre produtos, preços e transações, são puramente fictícios e não reais. Nenhuma transação real será realizada e nenhuma informação pessoal será coletada.</p>
+
+        <p className="text-xs">Ao acessar ou usar nosso site, você concorda em ficar vinculado a estes Termos de Serviço. Se você não concorda com estes Termos de Serviço, por favor, abstenha-se de usar nosso site.</p>
+
+        <h1 className="text-lg">Propriedade Intelectual</h1>
+
+        <p className="text-xs">O conteúdo do nosso site, incluindo texto, imagens, ícones, logotipos e gráficos, são propriedade livre. rocurando.</p>
+
+        <h1 className="text-lg">Isenção de Garantias</h1>
+
+        <p className="text-xs">Nós não fornecemos nenhuma garantia, expressa ou implícita, sobre as informações e materiais do nosso site. Embora nos esforcemos para manter nosso site atualizado e preciso, não garantimos que as informações em nosso site sejam 100% precisas, completas ou atuais.</p>
+
+        <h1 className="text-lg">Limitação de Responsabilidade</h1>
+
+        <p className="text-xs">Em nenhuma circunstância seremos responsáveis por qualquer tipo de dano decorrente ou relacionado ao uso do nosso site. Isso inclui, sem limitação, danos diretos ou indiretos, danos por perda de lucros, interrupção de negócios ou qualquer outro tipo de perda ou dano decorrente ou relacionado ao uso ou impossibilidade de uso do nosso site.</p>
+
+        <h1 className="text-lg">Lei Aplicável</h1>
+
+        <p className="text-xs">Estes Termos de Serviço serão regidos e interpretados de acordo com as leis do estado em que nossa empresa está registrada, sem dar efeito a quaisquer princípios de conflito de leis.</p>
+
+        <h1 className="text-lg">Alterações nos Termos de Serviço</h1>
+
+        <p className="text-xs">Reservamos o direito de revisar e atualizar estes Termos de Serviço a qualquer momento. Seu uso contínuo do nosso site após a publicação de quaisquer revisões a estes Termos de Serviço será considerado como sua aceitação dessas alterações.</p>
+
+        <h1 className="text-lg">Entre em Contato Conosco</h1>
+
+        <p className="text-xs">Se você tiver alguma dúvida sobre estes Termos de Serviço, entre em contato conosco.</p>
+
+        <p className="text-xs">Obrigado por usar nossa loja virtual de comércio eletrônico para fins de teste!</p>
+
+      </section>
+    </main>
+  )
+}
