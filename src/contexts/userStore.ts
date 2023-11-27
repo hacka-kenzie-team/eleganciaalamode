e48e272339940c3ecca92d8b72c0e061510379f5
svnoliveira <srkuran@gmail.com>
2023-11-26 22:56:03 -0300

    feat: fix erro de merge com develop

--- src/contexts/userStore.ts
+++ src/contexts/userStore.ts
@@ -17,8 +17,6 @@ export const userStore = create<IUserState>()((set, get) => ({
     userData: null,
     loading: false,
 
-    setMessage: (string) => { set({message: string})},
-    setError: (string) => { set({error: string})},
 
     setLoading: (boolean) => {
         set({loading: boolean})
